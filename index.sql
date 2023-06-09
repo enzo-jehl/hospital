41ffe745a90d91a4c065fbd7c83d216878e859d5
AlnyfoL <100089297+AlnyfoL@users.noreply.github.com>
2023-06-09 13:06:21 +0700

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,23 +1,28 @@
--- Création d'un index composite sur les colonnes patient_ID et doctor_ID de la table appointment
+-- Creation of a composite index on the patient_ID and doctor_ID columns of the appointment table
 CREATE INDEX idx_appointment_patient_doctor ON appointment(patient_ID, doctor_ID);
 
--- Création d'un index sur la colonne department_name de la table department
+-- Creation of an index on the department_name column of the department table
 CREATE INDEX idx_department_department_name ON department(department_name);
 
--- Création d'un index sur la colonne name de la table nurse
+-- Creation of an index on the name column of the nurse table
 CREATE INDEX idx_nurse_name ON nurse(name);
 
--- Création d'un index sur les colonnes name et contact_number de la table patient
+-- Creation of an index on the name and contact_number columns of the patient table
 CREATE INDEX idx_patient_name_contact_number ON patient(name, contact_number);
 
--- Création d'un index sur la colonne speciality de la table doctor
+-- Creation of an index on the specialty column of the doctor table
 CREATE INDEX idx_doctor_speciality ON doctor(speciality);
 
--- Création d'un index sur les colonnes patient_ID et admission_date de la table medical_record
+-- Creation of an index on the patient_ID and admission_date columns of the medical_record table
 CREATE INDEX idx_medical_record_patient_admission_date ON medical_record(patient_ID, admission_date);
 
--- Création d'un index sur la colonne medication_name de la table medication
+-- Creation of an index on the medication_name column of the medication table
 CREATE INDEX idx_medication_medication_name ON medication(medication_name);
 
--- Création d'un index sur les colonnes patient_ID et prescription_date de la table patient_medication
+-- Creation of an index on the patient_ID and prescription_date columns of the patient_medication table
 CREATE INDEX idx_patient_medication_patient_prescription_date ON patient_medication(patient_ID, prescription_date);
+
+--Indexing is an important technique to enhance database performance. Indexes accelerate search, join, and filtering operations by creating additional 
+--data structures that facilitate quick access to matching records. However, it's important to note that creating indexes can also impact the performance of
+--insertion, update, and deletion operations, as indexes need to be maintained during these operations. 
+--Therefore, it's necessary to strike a proper balance based on the specific needs of the database and executed queries.
